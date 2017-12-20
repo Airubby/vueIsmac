6eb8bc5581fba94090580df5e6f382d613c132ec
xiegj <xiegj@loncomip.com>
2017-12-20 16:22:17 +0800

    系统管理的通知页展示

--- src/page/system.vue
+++ src/page/system.vue
@@ -5,17 +5,47 @@
             <TopChangeInfo v-bind:topChangeInfo="top_items"></TopChangeInfo>
         </div>
         <div class="loncom_scroll_con loncom_right_changecon">
-            <div :class="{'loncom_active': top_items[0].loncom_active}">
-                1
-            </div>
+            <!--通知-->
+            <div :class="{'loncom_active': top_items[0].loncom_active}" class="loncom_public_table">
+                
+                <div class="loncom_system_inform loncom_public_shadow" v-for="item in inform_box">
+                    <div class="loncom_system_inform_title">
+                        <div class="loncom_fl">
+                            <span :class="item.topImgClass" class="loncom_system_informalarm"></span>
+                            <strong>{{item.topTitle}}</strong>
+                            <span><em>*</em>{{item.topDetail}}</span>
+                        </div>
+                        <div class="loncom_fr">
+                            <a href="javascript:;" class="loncom_search_btn">配置</a>
+                        </div>
+                    </div>
+                    <div class="loncom_system_inform_con">
+                        <div class="loncom_system_inform_box">
+                            <div>通知方式</div>
+                            <div><em :class="{'loncom_active':item.system_dx}" class="loncom_system_dx"></em><span>短信</span></div>
+                            <div><em :class="{'loncom_active':item.system_dh}" class="loncom_system_dh"></em><span>电话</span></div>
+                            <div><em :class="{'loncom_active':item.system_sg}" class="loncom_system_sg"></em><span>声光</span></div>
+                            <div><em :class="{'loncom_active':item.system_yj}" class="loncom_system_yj"></em><span>邮件</span></div>
+                        </div>
+                        <div class="loncom_system_inform_box">
+                            <div>通知用户</div>
+                            <div><em :class="{'loncom_active':item.system_gly}" class="loncom_system_gly"></em><span>管理员</span></div>
+                            <div><em :class="{'loncom_active':item.system_xtyh}" class="loncom_system_xtyh"></em><span>系统用户</span></div>
+                            <div><em :class="{'loncom_active':item.system_fxtyh}" class="loncom_system_fxtyh"></em><span>非系统用户</span></div>
+                        </div>
+                    </div>
+                </div>
 
-            <div :class="{'loncom_active':top_items[1].loncom_active}">
+            </div>
+            <!--用户-->
+            <div :class="{'loncom_active':top_items[1].loncom_active}" class="loncom_public_table">
                 2
             </div>
-
-            <div :class="{'loncom_active':top_items[2].loncom_active}">
+            <!--后台-->
+            <div :class="{'loncom_active':top_items[2].loncom_active}" class="loncom_public_table">
                 3
             </div>
+            <!--结束-->
         </div>
     </div>
 </template>
@@ -32,7 +62,8 @@ export default {
     
   },
   mounted() {
-
+      //超出滚动
+        scrollCon()
   },
    data() {
        return {
@@ -42,6 +73,25 @@ export default {
 　　　　　　　　　{select:'用户',loncom_active:false},
                 {select:'后台',loncom_active:false},
 　　　　　　　],
+            //通知：
+            inform_box:[
+                {
+                    topImgClass:'loncom_index_alarm4',topTitle:'紧急',topDetail:'备注：等级高于等于“紧急”的事件将按此规则通知。',
+                    system_dx:true,system_dh:false,system_sg:true,system_yj:true,system_gly:true,system_xtyh:true,system_fxtyh:false
+                },
+                {
+                    topImgClass:'loncom_index_alarm3',topTitle:'重要',topDetail:'备注：等级高于等于“重要”的事件将按此规则通知。',
+                    system_dx:false,system_dh:false,system_sg:true,system_yj:true,system_gly:true,system_xtyh:false,system_fxtyh:false
+                },
+                {
+                    topImgClass:'loncom_index_alarm2',topTitle:'一般',topDetail:'备注：等级高于等于“一般”的事件将按此规则通知。',
+                    system_dx:true,system_dh:false,system_sg:false,system_yj:false,system_gly:true,system_xtyh:true,system_fxtyh:false
+                },
+                {
+                    topImgClass:'loncom_index_alarm1',topTitle:'提示',topDetail:'备注：等级高于等于“提示”的事件将按此规则通知。',
+                    system_dx:true,system_dh:false,system_sg:false,system_yj:false,system_gly:false,system_xtyh:false,system_fxtyh:false
+                }
+            ]
        }
    },
    methods:{
